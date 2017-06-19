1047c839b6c61fc8b3bd7024bdffc925406b3c4b
dikshaduryodhan <dikshaduryodhan123@gmail.com>
2017-06-19 20:27:43 +0530

    Add files via upload

---
+++ process_login.php
@@ -0,0 +1,95 @@
+<?php session_start();
+	
+	if(!empty($_POST))
+	{
+		$msg="";
+		
+		if(empty($_POST['usernm']))
+		{
+			$msg[]="No such User";
+		}
+		
+		if(empty($_POST['pwd']))
+		{
+			$msg[]="Password Incorrect........";
+		}
+		
+		
+		if(!empty($msg))
+		{
+			echo '<b>Error:-</b><br>';
+			
+			foreach($msg as $k)
+			{
+				echo '<li>'.$k;
+			}
+		}
+		else
+		{
+			
+			
+		
+			$link=mysqli_connect("mysql.gebook1.svc","shardul","mote")or die("Can't Connect...");
+			
+			mysqli_select_db($link ,"shop" ) or die("Can't Connect to Database...");
+			
+			$unm=$_POST['usernm'];
+			
+			$q="select * from user where u_unm='$unm'";
+			$q1="select * from admin where a_unm='$unm'";
+			
+			$res=mysqli_query($link,$q) or die("wrong query");
+			
+			$row=mysqli_fetch_assoc($res);
+			$res1=mysqli_query($link,$q1) or die("wrong query");
+			
+			$row1=mysqli_fetch_assoc($res1);
+			
+			if(!empty($row))
+			{
+				if($_POST['pwd']==$row['u_pwd'])
+				{
+					$_SESSION=array();
+					$_SESSION['unm']=$row['u_unm'];
+					$_SESSION['uid']=$row['u_pwd'];
+					$_SESSION['status']=true;
+					header("location:index.php");
+				}
+					
+					
+				else
+				{
+					echo 'Incorrect Password....';
+				}
+			 }
+			else if(!empty($row1))
+			    {
+				  if($_POST['pwd']==$row1['a_pwd'])
+				  {
+					$_SESSION=array();
+					$_SESSION['unm']=$row1['a_unm'];
+					$_SESSION['uid']=$row1['a_pwd'];
+					$_SESSION['status']=true;
+					header("location:admin/index.php");
+					
+				  }
+				
+				  else
+				  {
+					echo 'Incorrect Password....';
+				  }
+			   }
+			   else
+			   {
+				echo 'Invalid User';
+			  }
+			
+		}
+	
+	}
+	else
+	{
+		header("location:index.php");
+	}
+			
+?>
